25b2e46493cf714d122ef1dffa7219478d8d96f4
Travis Cowx <travis.cowx@dcmail.ca>
2017-12-06 14:16:46 -0500

    Salary Filter Works
    Filter works

--- AuntRosiesBookkeeping/AuntRosiesBookkeeping/Views/ManageEmployeesView.xaml.cs
+++ AuntRosiesBookkeeping/AuntRosiesBookkeeping/Views/ManageEmployeesView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,5 +25,33 @@ namespace AuntRosiesBookkeeping.Views
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Previews input and filters input for only numbers,
+        /// one decimal point, and 2 decimal points after
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtSalary_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var salary = sender as TextBox;     // Stores the textbox as a variable
+
+            // Checks to see if there is a number to two decimal points
+            if (Regex.IsMatch(salary.Text, @"^[0-9]+\.[0-9]{2}$"))
+            {
+                e.Handled = !Regex.IsMatch(e.Text, "a^");   // Filters out any input
+            }
+            // Checks to see if there is a decimal point
+            else if(salary.Text.Contains("."))
+            {
+                e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");  // Allows only numbers 
+            }
+            // Otherwise
+            else
+            {
+                e.Handled = Regex.IsMatch(e.Text, "[^0-9.-]+");     // Allows numbers and decimals
+            }
+        }
+        
     }
 }

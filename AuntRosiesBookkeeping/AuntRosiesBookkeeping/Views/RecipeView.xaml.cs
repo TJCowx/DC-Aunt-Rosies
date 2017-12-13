efdbed9673e968933a2d47455f3fdcdca4cc5324
William Dolan <william.dolan@dcmail.ca>
2017-12-12 20:55:49 -0500

    UpDowns are now text boxes that only accept numbers. They have 1 as default

--- AuntRosiesBookkeeping/AuntRosiesBookkeeping/Views/RecipeView.xaml.cs
+++ AuntRosiesBookkeeping/AuntRosiesBookkeeping/Views/RecipeView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AuntRosiesBookkeeping.Views
@@ -22,6 +24,13 @@ namespace AuntRosiesBookkeeping.Views
         public RecipeView()
         {
             InitializeComponent();
+        }
+
+        private void txtQuant_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var quantity = sender as TextBox;     // Stores the textbox as a variable
+            //Tests if text is numeric
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");  // Allows only numbers 
         }
     }
 }
--- AuntRosiesBookkeeping/AuntRosiesBookkeeping/Views/TransactionView.xaml.cs
+++ AuntRosiesBookkeeping/AuntRosiesBookkeeping/Views/TransactionView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,13 @@ namespace AuntRosiesBookkeeping.Views
         public TransactionView()
         {
             InitializeComponent();
+        }
+
+        private void txtQuant_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var quantity = sender as TextBox;     // Stores the textbox as a variable
+            //Tests if text is numeric
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");  // Allows only numbers 
         }
     }
 }

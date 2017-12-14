dcafc2f200caf9951b014a7e7f8dd6733342d306
Travis Cowx <tcowx21@gmail.com>
2017-12-14 18:52:06 -0500

    LISTVIEW WORKS
    https://stackoverflow.com/questions/3539547/display-datatable-in-listview-control   has WPF stuff that also needs to be inputted

--- AuntRosiesBookkeeping/AuntRosiesBookkeeping/Views/RecipeView.xaml.cs
+++ AuntRosiesBookkeeping/AuntRosiesBookkeeping/Views/RecipeView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -21,6 +23,12 @@ namespace AuntRosiesBookkeeping.Views
     /// </summary>
     public partial class RecipeView : Window
     {
+        #region DECLARATIONS
+        private aunt_rosieDataSet auntRosieDataset;
+        private aunt_rosieDataSetTableAdapters.inventoryTableAdapter inventoryTableAdapter;
+        private aunt_rosieDataSetTableAdapters.product_recipesTableAdapter productRecipiesTableAdapter;
+        #endregion
+
         public RecipeView()
         {
             InitializeComponent();
@@ -31,6 +39,71 @@ namespace AuntRosiesBookkeeping.Views
             var quantity = sender as TextBox;     // Stores the textbox as a variable
             //Tests if text is numeric
             e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");  // Allows only numbers 
+        }
+
+        /// <summary>
+        /// On form loaded populate the listboxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmRecipeView_Loaded(object sender, RoutedEventArgs e)
+        {
+            auntRosieDataset = new aunt_rosieDataSet();
+            inventoryTableAdapter = new aunt_rosieDataSetTableAdapters.inventoryTableAdapter();
+            inventoryTableAdapter.Fill(auntRosieDataset.inventory);
+
+            lstIngredients.ItemsSource = auntRosieDataset.inventory;        // Fills the list from the data that is needed
+            // Load the ingredients listview
+            // Populate the columns
+
+
+            // Empty the recipe listview
+
+        }
+
+        /// <summary>
+        /// Add the selected ingredient to the recipe
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAddIngredient_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Add ingredient to the recipe listbox
+        }
+
+        /// <summary>
+        /// Remove the selected recipe ingredient 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRemoveIngredient_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Remove ingredient from recipe listbox
+        }
+
+        /// <summary>
+        /// Remove all ingredients from the recipe table
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRemoveAllIngredients_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Clear recipe listbox
+        }
+
+        /// <summary>
+        /// Save the recipe and return to the previous menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            
+            // TODO: SQL Query to change/update the recipe
+
+            String sql = "UPDATE ";    // SQL command to be executed
+
+            this.Close();   // Close this box
         }
     }
 }
